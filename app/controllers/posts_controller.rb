b017925baeacdad23af14ff1377183748d5ca4b7
Pan Cheng <pchengeducation@hotmail.com>
2018-08-29 01:57:30 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,41 +1,25 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
-
   def show
-  end
-
-  def new
-    @post = Post.new
-  end
-
-  def create
-    @post = Post.new(post_params)
-    if @post.valid?
-      @post.save
-      redirect_to post_path(@post)
-    else
-      render :new
-    end
+    @post = Post.find(params[:id])
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
     if @post.update(post_params)
-      redirect_to post_path(@post)
-    else
-      render :edit
-    end
+    
+    redirect_to post_path(@post)
+  else 
+    render :edit
   end
+end
 
   private
 
-    def post_params
-      params.permit(:title, :category, :content)
-    end
-
-    def set_post
-      @post = Post.find(params[:id])
-    end
+  def post_params
+    params.permit(:title, :category, :content)
+  end
 end
